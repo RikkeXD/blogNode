8634d1c46f0664a4b91af4cc56ec2567e27409ad
RikkeXD <87154168+RikkeXD@users.noreply.github.com>
2023-03-24 08:49:59 -0300

    Update link banco de dados

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://rikkeadmin:D7rZjPy59SenPTm2@blogapp.54e48oz.mongodb.net/?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://rikkeadmin:D7rZjPy59SenPTm2@blogapp.54e48oz.mongodb.net/test'}
 }else{
     module.exports = {mongoURI: 'mongodb://127.0.0.1/blogapp'}
 }
